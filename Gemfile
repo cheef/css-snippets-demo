0b4d19ff57c4fee6e478a64bf069c3946307d297
Ivan Garmatenko <cheef.che@gmail.ru>
2012-02-24 13:23:10 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :rubygems
+
+gem 'rails', '3.2.1'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'therubyracer'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'compass-rails'
+gem 'compass_twitter_bootstrap'
+gem 'css-snippets', :path => '../css-snippets'
+gem 'high_voltage'
+gem 'faker'
+
+group :development do
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CssSnippetsDemo::Application.config.secret_token = '23d2d8a2808ebc84a5d320cec892bf47915b1a53e2c292fbc0f94079333a6d48b324f08ffdd17e0ae7f065f5edaf06dcc212dac2bd2115b3923da055d38e26c5'
